70b128960c44d33c9ad1dc2dccf09db460c7dd30
Sergey Koltsov <ringov@yandex-team.ru>
2019-05-19 21:20:16 +0300

    Init commit.

---
+++ app/src/main/java/com/ringov/tnknws/App.kt
@@ -0,0 +1,41 @@
+package com.ringov.tnknws
+
+import android.app.Application
+import android.os.StrictMode
+import com.squareup.leakcanary.LeakCanary
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+        LeakCanary.install(this)
+        initStrictMode()
+
+        Logger.d("App created")
+    }
+
+    private fun initStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                StrictMode.ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDropBox()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                StrictMode.VmPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .penaltyDropBox()
+                    .build()
+            )
+        }
+    }
+
+}
---
+++ app/src/main/java/com/ringov/tnknws/Logger.kt
@@ -0,0 +1,41 @@
+package com.ringov.tnknws
+
+import android.util.Log
+
+class Logger {
+    companion object {
+        private const val LOGGER_TAG = "Logger"
+        private const val TAGS_SEPARATOR = "|"
+
+        fun d(message: String, vararg tags: String = arrayOf()) {
+            if (BuildConfig.DEBUG) {
+                log(null, message, tags) { _, m, t -> Log.i(t, m) }
+            }
+        }
+
+        fun w(message: String, vararg tags: String = arrayOf()) {
+            log(null, message, tags) { _, m, t -> Log.w(t, m) }
+        }
+
+        fun e(ex: Throwable, vararg tags: String = arrayOf()) {
+            log(ex, ex.message ?: "", tags) { e, m, t -> Log.e(t, m, e) }
+        }
+
+        private fun log(
+            throwable: Throwable? = null,
+            message: String,
+            tags: Array<out String>,
+            logger: (Throwable?, String, String) -> Unit
+        ) {
+            val allTags = if (tags.isNotEmpty()) {
+                val sb = StringBuilder()
+                sb.append(LOGGER_TAG).append(TAGS_SEPARATOR)
+                tags.forEach { sb.append(it).append(TAGS_SEPARATOR) }
+                sb.toString()
+            } else {
+                LOGGER_TAG
+            }
+            logger(throwable, message, allTags)
+        }
+    }
+}
